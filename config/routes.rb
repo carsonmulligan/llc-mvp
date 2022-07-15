935ab91aab756bf168323b9e4a784493615f6f2c
carson mulligan <carsonmulligan45@gmail.com>
2022-07-15 11:31:45 -0500

    root to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'products/index'
   get 'products/show'
-  root 'pages#landing'
+  root 'pages#home'
   get 'pages/home'
   get 'pages/landing'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
